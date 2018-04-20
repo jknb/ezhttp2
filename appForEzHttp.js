f136e041f68a945d6c7d540bbd733088e7bb85ad
jknb <alvertischris@gmail.com>
2018-04-20 17:25:52 +0300

    Add files via upload

---
+++ appForEzHttp.js
@@ -0,0 +1,28 @@
+const http = new EasyHTTP;
+
+// Get users
+// const users = http.get('https://jsonplaceholder.typicode.com/users')
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err));
+
+// User data
+const data = {
+    name: 'Chris alve',
+    username: '1337',
+    email: 'player@gmail.com'
+}
+
+// Create user
+// http.post('https://jsonplaceholder.typicode.com/users', data)
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err));
+
+// Update user
+// http.put('https://jsonplaceholder.typicode.com/users/2', data)
+//     .then(data => console.log(data))
+//     .catch(err => console.log(err));
+
+// Delete user
+http.delete('https://jsonplaceholder.typicode.com/users/2')
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
---
+++ ezhttp2.js
@@ -0,0 +1,69 @@
+/**
+ * ezHTTP Library
+ * Library for making http requests
+ * 
+ * @version 2.0.0
+ * @author 1337
+ * 
+ **/
+
+ class EasyHTTP {
+    // Make HTTP GET Request 
+    get(url) {
+        return new Promise((resolve, reject) =>{ 
+            fetch(url)
+            .then(res => res.json())
+            .then(data => resolve(data))
+            .catch(err => reject(err));
+        });   
+    }
+
+    // Make an HTTP POST Request
+    post(url) {
+        return new Promise((resolve, reject) =>{ 
+            fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            })
+            .then(res => res.json())
+            .then(data => resolve(data))
+            .catch(err => reject(err));
+        });   
+    }
+
+    // Make an HTTP PUT Request
+    put(url) {
+        return new Promise((resolve, reject) =>{ 
+            fetch(url, {
+                method: 'PUT',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            })
+            .then(res => res.json())
+            .then(data => resolve(data))
+            .catch(err => reject(err));
+        });   
+    }
+
+    // Make HTTP DELETE Request 
+    delete(url) {
+        return new Promise((resolve, reject) =>{ 
+            fetch(url, {
+                method: 'DELETE',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+            })
+            .then(res => res.json())
+            .then(() => resolve('Resource deleted'))
+            .catch(err => reject(err));
+        });   
+    }
+
+
+ }
